a6c724a7991853c6f9a9ffe645f6cc468fecb6cd
Andrew Arnott <andrewarnott@live.com>
2025-06-29 12:51:05 -0600

    touch-ups

--- test/SdkConsumer/Run-Tests.ps1
+++ test/SdkConsumer/Run-Tests.ps1
@@ -5,7 +5,7 @@ if ($LASTEXITCODE -eq 0) {
 	# Set env var so that nuget.config in this directory can resolve the SDK we built locally.
 	if (!$env:BUILDCONFIGURATION) { $env:BUILDCONFIGURATION = 'Debug' }
 
-	dotnet build
+	dotnet build -bl
 }
 
 Pop-Location
